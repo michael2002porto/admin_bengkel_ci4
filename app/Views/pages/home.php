9d415dd8dab945a097ae7c74414508fbbc6441e5
Michael Natanael <mnatanael87@gmail.com>
2023-05-20 20:58:08 +0700

    Fix gitignore

--- app/Views/pages/home.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend('layouts/main') ?>
-
-<?= $this->section('content') ?>
-<div class="card rounded-0">
-    <div class="card-body">
-    <h1 class="fw-bold">Welcome, <?= $session->get('login_name') ?>!</h1>
-    </div>
-</div>
-<?= $this->endSection() ?>
